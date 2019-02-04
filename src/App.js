50df785ae23ff4dc1261ecb5c63d060b6721c4cb
Tendai Prince Dzonga <ptdzonga@gmail.com>
2019-02-03 21:45:31 -0600

    initial setup + nav header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HeaderNav from './containers/HeaderNav/HeaderNav'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <HeaderNav />
+      </div>
+    );
+  }
+}
+
+export default App;
